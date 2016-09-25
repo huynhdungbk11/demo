11b7ae1f7b26fc17744052795661edaf4f71cd7c
huynhdung <huynhdungbk11@gmail.com>
2016-09-25 22:21:18 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,11 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :entries, only: [:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
+
 end
